6b115ccaa4423dc3b4562767adfdf23a825f6a20
Kelly James <kellyjamesclancy@gmail.com>
2016-10-06 03:27:46 +0000

    Code refactored, added code for formating out, added commenting to explain each part.

--- count-letters.js
+++ count-letters.js
@@ -1,42 +1,29 @@
+// taking user input and converting it to a string
 var userInput = process.argv.slice(2);
 var word = userInput.join('');
 
-// console.log(word);
-
+// for loop creating an object with key/value pairs where key
+// is letter and value is the count
 function countLetters(word) {
   var object = {};
-  for(var i = 0; i < word.length; i++) {
+  for (var i = 0; i < word.length; i++) {
     var letter = word.charAt(i);
-    if(object[letter]) {
-      object[letter] += 1;
-    } else {
+    if (!object[letter]) {
       object[letter] = 1;
+    } else {
+      object[letter] += 1;
     }
   }
   return object;
 }
 
-console.log(countLetters(word));
+// formating output
+var letterObj = countLetters(word);
+console.log('{');
+for (var objKey in letterObj) {
+  var objValue = letterObj[objKey];
+  console.log('  ' + objKey + ': ' + objValue + ',');
+}
+console.log('}');
 
 
-// console.log(object);
-
-// var word = userInput["input"];
-// var wordObj = {char: word.split('')};
-// var letter = wordObj["char"];
-
-// console.log(typeof wordObj);
-
-// console.log(word);
-
-// function countLetters(userInput) {
-//   for(var i = 0; i < word.length; i++) {
-//     letterArray.push(word.slice(i, i + 1));
-
-//     // console.log(word[i]);
-//   }
-//     return letterArray;
-// }
-
-// console.log(countLetters());
-// console.log(letterArray);
